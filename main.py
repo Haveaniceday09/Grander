7c07824b94da469f39cef0b1355afed578c89fb8
Ellis <73247831+Haveaniceday09@users.noreply.github.com>
2022-07-06 14:56:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from PIL import Image, ImageDraw, ImageFont
 import codecs
 
-persons = codecs.open('sources/names.txt', encoding="utf8")
-texts = codecs.open('sources/words.txt', encoding="utf8")
+namestxt = codecs.open('sources/names.txt', encoding="utf8")
+wordstxt = codecs.open('sources/words.txt', encoding="utf8")
 font = ImageFont.truetype('sources/Montserrat-Black.ttf', size=32)
-template = Image.open('template/temp.jpg')
-for pers in persons:
+
+names = []
+words = []
+
+for i in namestxt:
+	names.append(i.strip())
+
+for i in wordstxt:
+	words.append(i.strip())
+
+for pers in names:
+	template = Image.open('template/temp.jpg')
 	draw_text = ImageDraw.Draw(template)
 	draw_text.text((218,450), f'{pers}', font=font, fill=(0,0,0))
-	template.save(pers.strip() + '.jpg')
-	for i in texts:
-		current = Image.open(pers.strip() + '.jpg')
+	template.save(pers + '.jpg')
+	for i in words:
+		current = Image.open(pers + '.jpg')
 		new = ImageDraw.Draw(current)
 		new.text((200,500), f'{i}', font=font, fill=(0,0,0))
-		current.save(pers.strip() + ' ' + i + '.jpg')
+		current.save(pers + ' ' + i + '.jpg')
